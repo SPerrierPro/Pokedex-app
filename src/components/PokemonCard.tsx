8258310069b34ee5114bfcafc1e73d972f0ffd46
SPerrierPro <sophie.perrier.pro@gmail.com>
2025-03-31 15:13:15 +0200

    Added mew and an image alternative

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,11 +1,25 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
+  const pokemon = pokemonList[0];
+  const imageOrNot = pokemon.imgSrc ? (
+    <img src={pokemon.imgSrc} alt={pokemon.name} />
+  ) : (
+    <p>"???"</p>
+  );
   return (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbasaur"
-      />
-      <figcaption>Bulbasaur</figcaption>
+      {imageOrNot}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
