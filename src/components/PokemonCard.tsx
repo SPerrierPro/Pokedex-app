6d64034053813a798e1fa6358053ba2e89e28e47
SPerrierPro <sophie.perrier.pro@gmail.com>
2025-03-31 16:32:23 +0200

    Used props to display pokemon

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,24 +1,11 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-function PokemonCard() {
-  const pokemon = pokemonList[0];
-  const imageOrNot = pokemon.imgSrc ? (
-    <img src={pokemon.imgSrc} alt={pokemon.name} />
-  ) : (
-    <p>"???"</p>
-  );
+function PokemonCard({ pokemon }) {
   return (
     <figure>
-      {imageOrNot}
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>"???"</p>
+      )}
       <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
