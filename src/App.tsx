8fa884e9bc37e0a6f5f3028a2e31bead10bd35ef
SPerrierPro <sophie.perrier.pro@gmail.com>
2025-04-01 13:56:07 +0200

    Added buttons to switch between Pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 
 import PokemonCard from "./components/PokemonCard";
 
@@ -14,9 +15,23 @@ const pokemonList = [
 ];
 
 function App() {
+  const [pokemonName, setPokemonName] = useState("bulbasaur");
+
+  const pokemon = pokemonList.find((pokemon) => pokemon.name === pokemonName);
+
+  if (pokemon == null) {
+    throw new Error("Invalid pokemon name");
+  }
+
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemon} />
+      <button type="button" onClick={() => setPokemonName("bulbasaur")}>
+        bulbasaur
+      </button>
+      <button type="button" onClick={() => setPokemonName("mew")}>
+        mew
+      </button>
     </div>
   );
 }
